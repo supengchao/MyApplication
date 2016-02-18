c598c7dd5fa6f05c58df081bd01d2242740c1e4d
supengchao <1459384963@qq.com>
2016-02-18 15:13:22 +0800

    first commit

---
+++ app/src/main/java/com/example/supengchao/myapplication/Utils.java
@@ -0,0 +1,10 @@
+package com.example.supengchao.myapplication;
+
+/**
+ * Created by supengchao on 2016/2/18.
+ */
+public class Utils {
+    public static String getTestString(){
+        return "test";
+    }
+}
